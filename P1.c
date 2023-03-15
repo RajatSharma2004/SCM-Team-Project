c3b4ac7dd702504aefb1794b6a9df1c137e2329b
rohanlakhanpal <123047259+rohanlakhanpal@users.noreply.github.com>
2023-03-15 22:13:35 +0530

    Second commit

--- P1.c
+++ P1.c
@@ -2,24 +2,32 @@
 #include <stdlib.h>
 #include <time.h>
 
-int main() {
+int main() 
+{
     int number, guess, count = 0;
     srand(time(0)); // seed for random number generator
     number = rand() % 100 + 1; // generates a random number between 1 and 100
     printf("Guess the number between 1 and 100\n");
 
-    do {
-        printf("Enter your guess: ");
+    do 
+	{
+        printf("Enter the guessed number : ");
         scanf("%d", &guess);
         count++;
-        if (guess > number) {
-            printf("Too high! Try again.\n");
-        } else if (guess < number) {
-            printf("Too low! Try again.\n");
-        } else {
-            printf("Congratulations! You guessed the number in %d tries.\n", count);
+        if (guess > number) 
+		{
+            printf("The number is very large, please try again.\n");
+        } 
+		else if (guess < number) 
+		{
+            printf("The number is very small, please try again.\n");
+        } 
+		else 
+		{
+            printf("Congratulations! You guessed the number in %d tries.\n\a", count);
         }
-    } while (guess != number);
+    } 
+	while (guess != number);
 
     return 0;
 }
